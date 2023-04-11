da687e67049dccbd12225514144eda35609cf62b
Muhammad Daniyal Khan Durrani <daniyalkhand16@gmail.com>
2023-04-11 19:31:21 +0500

    Complete HTML and CSS for task 10

---
+++ Task 10 TODOS Web App/app.js
@@ -0,0 +1,6 @@
+// DOM Elements
+const todoForm = document.querySelector('#todo-form');
+const todoList = document.querySelector('.todos');
+const totalTasks = document.querySelector('#total-tasks');
+const remainingTasks = document.querySelector('#remaining-tasks');
+const completedTasks = document.querySelector('#completed-tasks');
